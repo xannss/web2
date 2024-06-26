7555e8d963a2c8e496bdd24e2a870c879badfcef
farisrizqi865 <farisrizqi865@gmail.com>
2024-06-26 11:05:08 +0700

    menambahka route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(authController::class)->group(function (){
+    Route::get('index','index',)->name('index');
 });
