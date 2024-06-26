a7a3db9b4198882a9a453871fde0a69a00a71c4b
Fahmi503 <fahmi.firmansyah503@gmail.com>
2024-06-26 19:46:56 +0700

    create blade, pruduck conttroler

--- app/Http/Controllers/produtccontroller.php
+++ app/Http/Controllers/produtccontroller.php
@@ -19,7 +19,7 @@ class produtccontroller extends Controller
      */
     public function create()
     {
-        //
+        return view('auth.create');
     }
 
     /**
---
+++ resources/views/auth/create.blade.php
@@ -0,0 +1,28 @@
+ <!-- START FORM -->
+ <form action='' method='post'>
+        <div class="my-3 p-3 bg-body rounded shadow-sm">
+            <div class="mb-3 row">
+                <label for="nim" class="col-sm-2 col-form-label">Kode Produk</label>
+                <div class="col-sm-10">
+                    <input type="number" class="form-control" name='nim' id="nim">
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="nama" class="col-sm-2 col-form-label">Nama Produk</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name='nama' id="nama">
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="jurusan" class="col-sm-2 col-form-label">Harga</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" name='jurusan' id="jurusan">
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="jurusan" class="col-sm-2 col-form-label"></label>
+                <div class="col-sm-10"><button type="submit" class="btn btn-primary" name="submit">SIMPAN</button></div>
+            </div>
+          </form>
+        </div>
+        <!-- AKHIR FORM -->
