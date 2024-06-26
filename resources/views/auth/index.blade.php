03469a5e8f82ad389d56a826550149ad1af2119d
Fahmi503 <fahmi.firmansyah503@gmail.com>
2024-06-26 23:19:46 +0700

    reply error

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -1,6 +1,14 @@
-@extends('layout.template')
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Data Produk</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  </head>
+  <body class="bg-light">
+    <main class="container">
         <!-- START DATA -->
-        @section('konten')
         <div class="my-3 p-3 bg-body rounded shadow-sm">
                 <!-- TOMBOL TAMBAH DATA -->
                 <div class="pb-3">
@@ -30,6 +38,10 @@
                         </tr>
                     </tbody>
                 </table>
+               
           </div>
           <!-- AKHIR DATA -->
-          @endsection
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+</html>
